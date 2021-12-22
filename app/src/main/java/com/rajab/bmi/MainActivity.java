2478ef15a50ffc0a4be1fa6e7eb6d7384919387b
Rajab Aabed <rajab7abed@gmail.com>
2021-12-22 21:51:51 +0200

    first commit

---
+++ app/src/main/java/com/rajab/bmi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rajab.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calcAge(View view) {
+        EditText value = (EditText) findViewById(R.id.editTextText1);
+        int v = Integer.parseInt(value.getText().toString());
+        v = 2021 - v;
+        Toast.makeText(this,"age: " + String.valueOf(v),Toast.LENGTH_LONG).show();
+    }
+}
